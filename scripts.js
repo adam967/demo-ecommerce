b6b9a5d2a667810842ef2829ff7c916ca3e14b94
adam967 <45872136+adam967@users.noreply.github.com>
2024-12-18 23:26:55 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,3 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
         expandButton.textContent = hiddenDescription.style.display === 'inline' ? 'Zwiń opis' : 'Rozwiń opis';
     });
 });
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('a'); // Selekcja wszystkich linków na stronie
+  
+  links.forEach(link => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault(); // Zapobiega domyślnemu zachowaniu linku
+      const url = this.getAttribute('href');
+      window.location.href = url; // Przejście do linku z twardym przeładowaniem
+    });
+  });
+});
