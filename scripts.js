6333652e82f2146c89a841924bd77a149a3f1c0b
adam967 <45872136+adam967@users.noreply.github.com>
2024-12-24 08:23:22 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Rozwijanie i zwijanie opisu
     const expandButton = document.querySelector('.expand-description');
     const hiddenDescription = document.querySelector('.hidden-description');
 
-    expandButton.addEventListener('click', () => {
-        hiddenDescription.style.display = hiddenDescription.style.display === 'inline' ? 'none' : 'inline';
-        expandButton.textContent = hiddenDescription.style.display === 'inline' ? 'Zwiń opis' : 'Rozwiń opis';
-    });
-});
+    if (expandButton && hiddenDescription) {
+        expandButton.addEventListener('click', () => {
+            hiddenDescription.style.display = hiddenDescription.style.display === 'inline' ? 'none' : 'inline';
+            expandButton.textContent = hiddenDescription.style.display === 'inline' ? 'Zwiń opis' : 'Rozwiń opis';
+        });
+    }
 
-
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  const links = document.querySelectorAll('a'); // Selekcja wszystkich linków na stronie
+    // Obsługa linków
+    const links = document.querySelectorAll('a'); // Selekcja wszystkich linków na stronie
   
-  links.forEach(link => {
-    link.addEventListener('click', function (event) {
-      event.preventDefault(); // Zapobiega domyślnemu zachowaniu linku
-      const url = this.getAttribute('href');
-      window.location.href = url; // Przejście do linku z twardym przeładowaniem
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault(); // Zapobiega domyślnemu zachowaniu linku
+            const url = link.getAttribute('href');
+            if (url) {
+                window.location.href = url; // Przejście do linku z twardym przeładowaniem
+            }
+        });
     });
-  });
 });
