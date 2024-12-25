33bbd1758d630c13d338b2ee1a661c7da8753446
adam967 <45872136+adam967@users.noreply.github.com>
2024-12-25 11:16:32 +0100

    Update klaro-config.js

--- klaro-config.js
+++ klaro-config.js
@@ -1,16 +1,39 @@
 var klaroConfig = {
-  elementID: 'klaro', // ID elementu, w którym będzie działał Klaro
+  elementID: 'klaro', // ID elementu, w którym będzie wyświetlany baner
   version: 1, // Wersja konfiguracji
-  cookieName: 'klaro-consent', // Nazwa pliku cookie przechowującego preferencje
+  cookieName: 'klaro-consent', // Nazwa cookie przechowującego zgodę
   privacyPolicy: '/privacy-policy', // Link do polityki prywatności
-  default: false, // Czy wszystkie usługi są domyślnie włączone
+  default: false, // Domyślna konfiguracja - usługi nieaktywne
+  position: 'bottom', // Miejsce wyświetlania banera (góra/dół)
+  buttons: {
+    acceptAll: {
+      text: 'Akceptuj wszystkie', // Tekst przycisku "Akceptuj wszystkie"
+      callback: function() {
+        klaro.acceptAll(); // Funkcja akceptująca wszystkie zgody
+      }
+    },
+    preferences: {
+      text: 'Wybierz ustawienia', // Tekst przycisku "Wybierz ustawienia"
+      callback: function() {
+        klaro.show(); // Funkcja wyświetlająca panel wyboru
+      }
+    }
+  },
   apps: [
     {
       name: 'google-analytics',
       title: 'Google Analytics',
       purposes: ['analytics'],
-      cookies: ['_ga', '_gid'], // Nazwy plików cookie związanych z usługą
+      cookies: ['_ga', '_gid'], // Pliki cookie
       required: false, // Czy zgoda jest wymagana
     },
-  ],
+    {
+      name: 'facebook-pixel',
+      title: 'Facebook Pixel',
+      purposes: ['analytics'],
+      cookies: ['_fbp'],
+      required: false,
+    },
+    // Dodaj inne aplikacje wg potrzeb
+  ]
 };
