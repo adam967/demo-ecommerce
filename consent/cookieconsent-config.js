c64c26289fc404ee2050b725e02e821b2075c6cb
adam967 <45872136+adam967@users.noreply.github.com>
2024-12-27 10:09:22 +0100

    Update cookieconsent-config.js

--- consent/cookieconsent-config.js
+++ consent/cookieconsent-config.js
@@ -24,7 +24,7 @@ CookieConsent.run({
     guiOptions: {
         consentModal: {
             layout: 'cloud inline',
-            position: 'bottom center',
+            position: 'middle center',
             equalWeightButtons: true,
             flipButtons: false
         },
